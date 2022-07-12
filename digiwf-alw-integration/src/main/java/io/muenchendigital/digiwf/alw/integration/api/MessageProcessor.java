27da1c2143e515b09a0b70449fd2bcc89e9b85db
martin.dietrich <martin.dietrich@muenchen.de>
2022-07-12 11:47:00 +0200

    first draft (not working)

--- digiwf-alw-integration/src/main/java/io/muenchendigital/digiwf/alw/integration/api/MessageProcessor.java
+++ digiwf-alw-integration/src/main/java/io/muenchendigital/digiwf/alw/integration/api/MessageProcessor.java
@@ -7,8 +7,10 @@ package io.muenchendigital.digiwf.alw.integration.api;
 import io.muenchendigital.digiwf.alw.integration.domain.model.AlwPersoneninfoRequest;
 import io.muenchendigital.digiwf.alw.integration.domain.model.AlwPersoneninfoResponse;
 import io.muenchendigital.digiwf.alw.integration.domain.service.AlwPersoneninfoService;
+import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.dto.ErrorDto;
 import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.infrastructure.RoutingCallback;
 import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.service.CorrelateMessageService;
+import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.service.ErrorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.function.context.MessageRoutingCallback;
@@ -16,6 +18,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Sinks;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +32,9 @@ public class MessageProcessor { // TODO remove
     public static final String TYPE_HEADER_GET_ALW_ZUSTAENDIGKEIT_EVENT_BUS = "getAlwZustaendigkeitEventBus";
     private final AlwPersoneninfoService alwPersoneninfoService;
     private final CorrelateMessageService correlateMessageService;
+    private final ErrorService errorService;
     private static final String ALW_ZUSTAENDIGE_GRUPPE = "alwZustaendigeGruppe";
+    private final Sinks.Many<Message<ErrorDto>> errorSink;
 
     /**
      * Override the custom router of the digiwf-spring-cloudstream-utils. We only have one type we need to map.
@@ -55,10 +60,11 @@ public class MessageProcessor { // TODO remove
             log.debug("Request: {}", alwPersoneninfoRequest);
             try {
                 final AlwPersoneninfoResponse response = alwPersoneninfoService.getZustaendigkeit(alwPersoneninfoRequest);
-                emitResponse(message.getHeaders(), response);
+//                emitResponse(message.getHeaders(), response);
+                throw new RuntimeException("myexception"); // TODO
             } catch (final Exception e) {
                 log.error("Request could not be fulfilled: {}", e.getMessage());
-                emitResponse(message.getHeaders(), new AlwPersoneninfoResponse(null));
+                emitError(message.getHeaders(), "999", "TODO");
             }
         };
     }
@@ -73,5 +79,14 @@ public class MessageProcessor { // TODO remove
         final Map<String, Object> correlatePayload = new HashMap<>();
         correlatePayload.put(ALW_ZUSTAENDIGE_GRUPPE, alwPersoneninfoResponse.getZustaendigeGruppe());
         correlateMessageService.sendCorrelateMessage(messageHeaders, correlatePayload);
+    }
+
+    /**
+     * Function to emit a reponse using the correlateMessageService of digiwf-spring-cloudstream-utils
+     *
+     * @param messageHeaders          The MessageHeaders of the incoming message you want to correlate your answer to
+     */
+    public void emitError(final MessageHeaders messageHeaders, final String errorCode, final String errorMessage) {
+        errorService.sendError(messageHeaders, errorCode, errorMessage);
     }
 }
